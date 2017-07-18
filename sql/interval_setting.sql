b1fdc937bfc4271fbd9cd8b71bf608739aeab945
hellhker <pqc271662322@163.com>
2017-07-18 23:54:23 +0800

    Create interval_setting.sql

---
+++ sql/interval_setting.sql
@@ -0,0 +1,4 @@
+INSERT INTO `interval_setting` (`Record_ID`, `Factory`, `WorkShopNo`, `d_interval1`, `d_interval2`, `d_interval3`, `d_interval4`, `d_interval5`, `n_interval1`, `n_interval2`, `n_interval3`, `n_interval4`, `n_interval5`, `Weekend`, `Shift`) VALUES
+(1, NULL, '第四車間', '07:40-09:30', '09:40-11:20', '12:20-14:30', '14:40-17:00', '17:30', NULL, NULL, NULL, NULL, NULL, '0', 'D'),
+(2, NULL, '第四車間', '08:00-09:30', '09:30-11:20', '12:20-14:30', '14:30-17:00', '17:30', NULL, NULL, NULL, NULL, NULL, '1', 'D'),
+(3, NULL, '第四車間', '20:00-23:00', '23:40-00:50', '01:00-03:00', '03:10-05:00', '05:00-07:00', NULL, NULL, NULL, NULL, NULL, '0', 'N');
