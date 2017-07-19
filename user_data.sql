5fd4931db5c30f85e8676209c8662be6ef194df4
hellhker <pqc271662322@163.com>
2017-07-20 00:23:31 +0800

    add lineleader

---
+++ user_data.sql
@@ -0,0 +1,16 @@
+INSERT INTO `user_data`(`UserID`, `Password`, `ChineseName`, `DepartmentCode`)
+VALUES
+ (01394,'foxlink','楊霞','XR-07'),
+(208648,'foxlink','王正偉','XR-08'),
+(40908,'foxlink','羅春燕','XR-01'),
+(68951,'foxlink','任文彩','XR-40'),
+('KQ933','foxlink','石麗','XR-50'),
+('KV447','foxlink','段凱麗','XR-34'),
+('N6193','foxlink','張顯威','XR-27'),
+('R0405','foxlink','白云飛','XR-12'),
+('R5632','foxlink','孫燕','XR-56'),
+('U2782','foxlink','孟小述','XR-56'),
+('WL930','foxlink','付倩','XR-49'),
+('R1004','foxlink','謝建領','XR-31')
+
+
