f1bbd772a16677934c2135472c0c39131a96bffd
hellhker <pqc271662322@163.com>
2017-07-25 23:31:43 +0800

    Modify Level&delete count>1
    1、删除多余的
    2、备份指示单
    3、修改原有权限

--- auth.php
+++ auth.php
@@ -1,15 +1,15 @@
 <?php 
 session_start();
 
-$MYSQL_LOGIN = "root";
-$MYSQL_PASSWORD = "foxlink";
-$MYSQL_HOST = "192.168.65.230";
+// $MYSQL_LOGIN = "root";
+// $MYSQL_PASSWORD = "foxlink";
+// $MYSQL_HOST = "192.168.65.230";
 
-$mysqli = new mysqli($MYSQL_HOST,$MYSQL_LOGIN,$MYSQL_PASSWORD,"swipecard");
-$mysqli->query("SET NAMES 'utf8'");	 
-$mysqli->query('SET CHARACTER_SET_CLIENT=utf8');
-$mysqli->query('SET CHARACTER_SET_RESULTS=utf8'); 
-
+// $mysqli = new mysqli($MYSQL_HOST,$MYSQL_LOGIN,$MYSQL_PASSWORD,"swipecard");
+// $mysqli->query("SET NAMES 'utf8'");	 
+// $mysqli->query('SET CHARACTER_SET_CLIENT=utf8');
+// $mysqli->query('SET CHARACTER_SET_RESULTS=utf8'); 
+include("mysql_config.php");
 $userName = $_POST["username"];
 $password = $_POST["password"];
 
