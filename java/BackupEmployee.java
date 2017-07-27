8455475e9f233cac994455fb5869d84b406fe351
hellhker <pqc271662322@163.com>
2017-07-28 06:54:52 +0800

    V0728
    添加java的判定条件
    备份人员资料

--- java/BackupEmployee.java
+++ java/BackupEmployee.java
@@ -1,11 +1,11 @@
 package com.backup.employee;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.apache.commons.lang.ArrayUtils;
+import net.sf.json.JSONObject;
 
 public class BackupEmployee {
 
@@ -15,58 +15,59 @@ public class BackupEmployee {
 	static String sql4 = null;
 	static String sql_countNew = null;
 	static String sql_countOld = null;
-	
+
 	static DBHelper db1 = null;
 	static DBHelper db2 = null;
 	static DBHelper db3 = null;
 	static DBHelper db4 = null;
 	static DBHelper dbNew = null;
 	static DBHelper dbOld = null;
-	
-	
+
 	static ResultSet ret = null;
 	static int res;
-	 
-	
 
-	public static boolean contains(String[] arr, String targetValue){
-		 return ArrayUtils.contains(arr,targetValue);
+	public static boolean contains(String[] arr, String targetValue) {
+		return ArrayUtils.contains(arr, targetValue);
 	}
-	
+
 	@SuppressWarnings("null")
 	public static void main(String[] args) {
 		System.out.println("test");
-//		sql = "select * from view_emp rand() limit 100";// SQL语句
-//		sql1 = "select * from view_rybz_cabg";// SQL语句
-		
-		sql1 = "select * from testemployee1 order by id";// SQL语句
-		
-		sql2 = "select * from testemployee";//
-		
-		
-//		sql = "select * from view_emp where iccardid = '0313240578'";
-		//sql2 = "INSERT INTO `testemployee`(`ID`, `Name`, `depid`, `depname`,`cardid`,`direct`,`costID` ) VALUES (?,?,?,?,?,?,?)";
-//		sql2 = "INSERT INTO `testemployee`(`ID`, `Name`, `depid`, `depname`, `cardid`) VALUES ('133028','徐文勇','7933','策略-資源管理-數據-系統整合課','0313240578')";
-		//sql3 = "DELETE FROM `testemployee`";
-		//sql4 = "UPDATE `testemployee` SET `Permission` = '1' WHERE ID IN ('01394','208648','40908','68951','CY439','KQ933','KV447','N6193','R0405','R5632','G0956','403935','U2782','WL930','R1004','286943','T9220','D3702','K7804','N5400','58788','FE440','G8611','N6398','N6315','N5289','N3447','A5798','G6659','156339','03236','AW050','04664','F0025','K4230','F0660','KQ728','256176','DC705','U1939','K7380','194236','175418','565535','368364','N5397','426017')";
+		// sql = "select * from view_emp rand() limit 100";// SQL语句
+		// sql1 = "select * from view_rybz_cabg";// SQL语句
+
+		sql1 = "select * from testemployee order by id";// SQL语句
+
+		sql2 = "select * from testemployee1 order by id";//
+
+		// sql = "select * from view_emp where iccardid = '0313240578'";
+		// sql2 = "INSERT INTO `testemployee`(`ID`, `Name`, `depid`,
+		// `depname`,`cardid`,`direct`,`costID` ) VALUES (?,?,?,?,?,?,?)";
+		// sql2 = "INSERT INTO `testemployee`(`ID`, `Name`, `depid`, `depname`,
+		// `cardid`) VALUES
+		// ('133028','徐文勇','7933','策略-資源管理-數據-系統整合課','0313240578')";
+		// sql3 = "DELETE FROM `testemployee`";
+		// sql4 = "UPDATE `testemployee` SET `Permission` = '1' WHERE ID IN
+		// ('01394','208648','40908','68951','CY439','KQ933','KV447','N6193','R0405','R5632','G0956','403935','U2782','WL930','R1004','286943','T9220','D3702','K7804','N5400','58788','FE440','G8611','N6398','N6315','N5289','N3447','A5798','G6659','156339','03236','AW050','04664','F0025','K4230','F0660','KQ728','256176','DC705','U1939','K7380','194236','175418','565535','368364','N5397','426017')";
 		Connection conn = DBHelper.getConnection();
 		System.out.println("A");
-		PreparedStatement psNew = null;//最新资料
+		PreparedStatement psNew = null;// 最新资料
 		ResultSet rsNew = null;
-		
-		PreparedStatement psOld = null;//旧的资料
+
+		PreparedStatement psOld = null;// 旧的资料
 		ResultSet rsOld = null;
-		
+
 		ResultSet countNew = null;
 		ResultSet countOld = null;
-		
+
 		System.out.println("B");
-		sql_countNew = "select count(*) from testemployee1 order by id";
-		sql_countOld = "select count(*) from testemployee order by id";
-		db1 = new DBHelper(sql1);// 创建DBHelper对象 |SelectCRUD Create result Delete
+		sql_countNew = "select count(*) from testemployee order by id";
+		sql_countOld = "select count(*) from testemployee1 order by id";
+		db1 = new DBHelper(sql1);// 创建DBHelper对象 |SelectCRUD Create result
+									// Delete
 		db2 = new DBHelper(sql2);// 创建DBHelper对象 |Create
-		//db3 = new DBHelper(sql3);
-		//db4 = new DBHelper(sql4);
+		// db3 = new DBHelper(sql3);
+		// db4 = new DBHelper(sql4);
 		dbNew = new DBHelper(sql_countNew);
 		dbOld = new DBHelper(sql_countOld);
 		/**
@@ -75,74 +76,88 @@ public class BackupEmployee {
 
 		// 4.执行SQL语句
 		System.out.println("123");
-//		String[] uids = null;
-//		String[] unames = null;
-//		String[] udepnames = null;
-//		String[] udepids = null;
-//		String[] ucardids = null;
-//		String[] udirects = null;
-//		String[] ucostids = null;
-//		
-//		String[] uids2 = null;
-//		String[] unames2 = null;
-//		String[] udepnames2 = null;
-//		String[] udepids2 = null;
-//		String[] ucardids2 = null;
-//		String[] udirects2 = null;
-//		String[] ucostids2 = null;
-		
-		
-		int i=0;
+		// String[] uids = null;
+		// String[] unames = null;
+		// String[] udepnames = null;
+		// String[] udepids = null;
+		// String[] ucardids = null;
+		// String[] udirects = null;
+		// String[] ucostids = null;
+		//
+		// String[] uids2 = null;
+		// String[] unames2 = null;
+		// String[] udepnames2 = null;
+		// String[] udepids2 = null;
+		// String[] ucardids2 = null;
+		// String[] udirects2 = null;
+		// String[] ucostids2 = null;
+
+		int i = 0;
 		int j = 0;
 		try {
-			//db3.sta.executeUpdate();
-//			psNew = conn.prepareStatement(sql1);
-//			rsNew = psNew.executeQuery();// 执行查询
-			
+			// db3.sta.executeUpdate();
+			// psNew = conn.prepareStatement(sql1);
+			// rsNew = psNew.executeQuery();// 执行查询
+
 			rsNew = db1.sta.executeQuery();
 			rsOld = db2.sta.executeQuery();
-			
-			countNew =  dbNew.sta.executeQuery();
-			countOld =  dbOld.sta.executeQuery();
+
+			countNew = dbNew.sta.executeQuery();
+			countOld = dbOld.sta.executeQuery();
 			int newCount = 0;
 			int oldCount = 0;
 			while (countNew.next()) {
-				 newCount = countNew.getInt(1);
-				
-				
+				newCount = countNew.getInt(1);
+
 			}
-			
+
 			while (countOld.next()) {
-				 oldCount = countOld.getInt(1);
-				
+				oldCount = countOld.getInt(1);
+
 			}
-			
-//			rsNew.last();
-//			rsOld.last();
-//			int newCount = rsNew.getRow(); //获得ResultSet的总行数
-//			int oldCount = rsOld.getRow(); //获得ResultSet的总行数
-//			int n =0;
-//			rsNew.absolute(n);
-//			rsOld.absolute(n);
-//			rsNew.first();
-//			rsOld.first();
+
+			// rsNew.last();
+			// rsOld.last();
+			// int newCount = rsNew.getRow(); //获得ResultSet的总行数
+			// int oldCount = rsOld.getRow(); //获得ResultSet的总行数
+			// int n =0;
+			// rsNew.absolute(n);
+			// rsOld.absolute(n);
+			// rsNew.first();
+			// rsOld.first();
 			String[] uids = new String[newCount];
-			String[] unames =  new String[newCount];
-			String[] udepnames =  new String[newCount];
-			String[] udepids =  new String[newCount];
-			String[] ucardids =  new String[newCount];
-			String[] udirects =  new String[newCount];
-			String[] ucostids =  new String[newCount];
-			
+			// String[] unames = new String[newCount];
+			// String[] udepnames = new String[newCount];
+			// String[] udepids = new String[newCount];
+			// String[] ucardids = new String[newCount];
+			// String[] udirects = new String[newCount];
+			// String[] ucostids = new String[newCount];
+			JSONObject unames = new JSONObject();
+			JSONObject udepnames = new JSONObject();
+			JSONObject udepids = new JSONObject();
+			JSONObject ucardids = new JSONObject();
+			JSONObject udirects = new JSONObject();
+			JSONObject ucostids = new JSONObject();
+
 			String[] uids2 = new String[oldCount];
-			String[] unames2 = new String[oldCount];
-			String[] udepnames2 = new String[oldCount];
-			String[] udepids2 = new String[oldCount];
-			String[] ucardids2 = new String[oldCount];
-			String[] udirects2 = new String[oldCount];
-			String[] ucostids2 = new String[oldCount];
-			
-			
+			// String[] unames2 = new String[oldCount];
+			// String[] udepnames2 = new String[oldCount];
+			// String[] udepids2 = new String[oldCount];
+			// String[] ucardids2 = new String[oldCount];
+			// String[] udirects2 = new String[oldCount];
+			// String[] ucostids2 = new String[oldCount];
+			JSONObject unames2 = new JSONObject();
+			JSONObject udepnames2 = new JSONObject();
+			JSONObject udepids2 = new JSONObject();
+			JSONObject ucardids2 = new JSONObject();
+			JSONObject udirects2 = new JSONObject();
+			JSONObject ucostids2 = new JSONObject();
+
+			// String userID = "123";
+			// String employeeName = "test";
+			// JSONObject createrMember = new JSONObject();
+			// createrMember.put("userid", userID);
+			// createrMember.put("employeeName", employeeName);
 			while (rsNew.next()) {// 判断是否还有下一个数据
 				String uid = rsNew.getString(1);
 				String uname = rsNew.getString(2);
@@ -152,30 +167,34 @@ public class BackupEmployee {
 				String udirect = rsNew.getString(5);
 				String ucostid = rsNew.getString(7);
 				uids[i] = uid;
-				unames[i] = uname;
-				udepnames[i] = udepname;
-				udepids[i] = udepid;
-				ucardids[i] = ucardid;
-				udirects[i] = udirect;
-				ucostids[i] = ucostid;
+				unames.put(uid, uname);
+				udepnames.put(uid, udepname);
+				udepids.put(uid, udepid);
+				ucardids.put(uid, ucardid);
+				udirects.put(uid, udirect);
+				ucostids.put(uid, ucostid);
+				// unames[i] = uname;
+				// udepnames[i] = udepname;
+				// udepids[i] = udepid;
+				// ucardids[i] = ucardid;
+				// udirects[i] = udirect;
+				// ucostids[i] = ucostid;
 				i++;
-				
-				
-//				db2.sta.setString(1, uid);
-//				db2.sta.setString(2, uname);
-//				db2.sta.setString(3, udepname);
-//				db2.sta.setString(4, udepid);
-//				db2.sta.setString(5, ucardid);
-//				db2.sta.setString(6, udirect);
-//				db2.sta.setString(7, ucostid);
-//				res = db2.sta.executeUpdate();// 插入數據
-				System.out.println(uid +"\t"+ uname + "\t" + udepname + "\t"
-					+ udepid + "\t" + ucardid);
+
+				// db2.sta.setString(1, uid);
+				// db2.sta.setString(2, uname);
+				// db2.sta.setString(3, udepname);
+				// db2.sta.setString(4, udepid);
+				// db2.sta.setString(5, ucardid);
+				// db2.sta.setString(6, udirect);
+				// db2.sta.setString(7, ucostid);
+				// res = db2.sta.executeUpdate();// 插入數據
+//				System.out.println(uid + "\t" + uname + "\t" + udepname + "\t" + udepid + "\t" + ucardid);
 			}
-//			System.out.println(i);
+			// System.out.println(i);
 			j = 0;
-			
-			while(rsOld.next()){
+
+			while (rsOld.next()) {
 				String uid = rsOld.getString(1);
 				String uname = rsOld.getString(2);
 				String udepname = rsOld.getString(3);
@@ -183,59 +202,70 @@ public class BackupEmployee {
 				String ucardid = rsOld.getString(6);
 				String udirect = rsOld.getString(5);
 				String ucostid = rsOld.getString(7);
-				
 				uids2[j] = uid;
-				unames2[j] = uname;
-				udepnames2[j] = udepname;
-				udepids2[j] = udepid;
-				ucardids2[j] = ucardid;
-				udirects2[j] = udirect;
-				ucostids2[j] = ucostid;
+				unames2.put(uid, uname);
+				udepnames2.put(uid, udepname);
+				udepids2.put(uid, udepid);
+				ucardids2.put(uid, ucardid);
+				udirects2.put(uid, udirect);
+				ucostids2.put(uid, ucostid);
+
+				// unames2[j] = uname;
+				// udepnames2[j] = udepname;
+				// udepids2[j] = udepid;
+				// ucardids2[j] = ucardid;
+				// udirects2[j] = udirect;
+				// ucostids2[j] = ucostid;
 				j++;
 			}
-			int x=0;
-			int y =0;
-			for(int k=0;k<i;k++){
-				
-				if(contains(uids2,uids[k])==true){
-					x++;
-				}else{
+			int x = 0;
+			int y = 0;
+			for (int k = 0; k < i; k++) {
+
+				if (contains(uids2, uids[k]) == true) {
+					if (!unames2.get(uids[k]).equals(unames.get(uids[k]))
+							|| !udepnames2.get(uids[k]).equals(udepnames.get(uids[k]))
+							|| !udepids2.get(uids[k]).equals(udepids.get(uids[k]))
+							|| !ucardids2.get(uids[k]).equals(ucardids.get(uids[k]))
+							|| !udirects2.get(uids[k]).equals(udirects.get(uids[k]))
+							|| !ucostids2.get(uids[k]).equals(ucostids.get(uids[k]))) {
+						System.out.println(unames2.get(uids[k])+""+unames.get(uids[k]));
+						x++;
+
+					}
+
+				} else {
 					y++;
 				}
-				
+
 			}
 			System.out.println(x);
 			System.out.println(y);
-			
-			
-			
-			
-			//db4.sta.executeUpdate();
+
+			// db4.sta.executeUpdate();
 			// ret = db1.pst.executeQuery();// 执行语句，得到结果集
-//			while (ret.next()) {
-//				String uid = ret.getString(1);
-//				String ufname = ret.getString(2);
-//				db2.sta.setString(1, uid);
-//				db2.sta.setString(2, ufname);
-//				// res = db2.sta.executeUpdate();
-//				// String ulname = ret.getString(3);
-//				// String udate = ret.getString(4);
-//				// System.out.println(uid + "\t" + ufname + "\t" + ulname + "\t"
-//				// + udate);
-//				// System.out.println(uid + "\t" + ufname);
-//			}// 显示数据
-//				// res = db2.sta.executeUpdate();
-//			ret.close();
+			// while (ret.next()) {
+			// String uid = ret.getString(1);
+			// String ufname = ret.getString(2);
+			// db2.sta.setString(1, uid);
+			// db2.sta.setString(2, ufname);
+			// // res = db2.sta.executeUpdate();
+			// // String ulname = ret.getString(3);
+			// // String udate = ret.getString(4);
+			// // System.out.println(uid + "\t" + ufname + "\t" + ulname + "\t"
+			// // + udate);
+			// // System.out.println(uid + "\t" + ufname);
+			// }// 显示数据
+			// // res = db2.sta.executeUpdate();
+			// ret.close();
 			// res.close();
-			//db4.close();
+			// db4.close();
 			db1.close();// 关闭连接
 			db2.close();
-			//db3.close();
+			// db3.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-
 
 }
